d95dea0b2814229fe06616d654b449ac7c5e8fd7
elad3434 <elad3434@gmail.com>
2023-05-22 19:56:18 +0300

    start stage 5

---
+++ unittests/renderer/ImageWriterTest.java
@@ -0,0 +1,22 @@
+package renderer;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import primitives.*;
+import renderer.Camera;
+/**
+ * Testing ImageWriter Class by creating a 800x500 pixels picture
+ * of a net of 10x16 squares with two colors
+ * @author Elad & Ishay
+ *
+ */
+class ImageWriterTest {
+
+    @Test
+    public void writeToImageTest() {
+
+    }
+
+}
