2cfe2b7ddc968b1a3315a5a09a392f9c7136cd2a
irish1814 <101556047+irish1814@users.noreply.github.com>
2023-05-30 16:16:19 +0300

    continue stage 5 added all tests to findClosestPoint in RayTest class

--- unittests/primitives/RayTest.java
+++ unittests/primitives/RayTest.java
@@ -2,7 +2,6 @@ package primitives;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -29,7 +28,7 @@ class RayTest {
         // ============ Equivalence Partitions Tests ==============
 
         //TC01: Point in the middle of list
-        List<Point> points1 = new ArrayList<Point>();
+        List<Point> points1 = new LinkedList<>();
         points1.add(p2);
         points1.add(p1);
         points1.add(p3);
@@ -38,18 +37,18 @@ class RayTest {
         // =============== Boundary Values Tests ==================
 
         //TC01: List is empty
-        List<Point> points2 = new ArrayList<Point>();
+        List<Point> points2 = new LinkedList<>();
         assertNull(ray.findClosestPoint(points2), "Found point is not as the expected one");
 
         //TC02: Point in the beginning of list
-        List<Point> points3 = new ArrayList<Point>();
+        List<Point> points3 = new LinkedList<>();
         points3.add(p1);
         points3.add(p2);
         points3.add(p3);
         assertEquals(p1, ray.findClosestPoint(points3), "Found point is not as the expected one");
 
         //TC03: Point in the end of list
-        List<Point> points4 = new ArrayList<Point>();
+        List<Point> points4 = new LinkedList<>();
         points4.add(p2);
         points4.add(p3);
         points4.add(p1);
