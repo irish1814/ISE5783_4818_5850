ef4df6a3a90224c9f928f08b4bdbf753dea7751e
irish1814 <101556047+irish1814@users.noreply.github.com>
2023-05-30 16:14:58 +0300

    continue stage 5 added all tests to findClosestPoint in RayTest class

--- unittests/primitives/RayTest.java
+++ unittests/primitives/RayTest.java
@@ -2,6 +2,7 @@ package primitives;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -18,11 +19,40 @@ class RayTest {
     /**
      * Test method for {@link primitives.Ray#findClosestPoint(List)}.
      */
-
     @Test
     void findClosestPoint() {
-        Ray ray = new Ray(new Vector(1, 2, 3), new Point(1, 0, 0));
-        List<Point> l = new LinkedList<>();
-        assertNull(ray.findClosestPoint(l));
+        Point p1 = new Point(0, 0, 1);
+        Point p2 = new Point(0, 0, 2);
+        Point p3 = new Point(0, 0, 3);
+        Ray ray = new Ray(new Vector(0, 0, 1), new Point(0, 0, 0.5));
+
+        // ============ Equivalence Partitions Tests ==============
+
+        //TC01: Point in the middle of list
+        List<Point> points1 = new ArrayList<Point>();
+        points1.add(p2);
+        points1.add(p1);
+        points1.add(p3);
+        assertEquals(p1, ray.findClosestPoint(points1), "Found point is not as the expected one");
+
+        // =============== Boundary Values Tests ==================
+
+        //TC01: List is empty
+        List<Point> points2 = new ArrayList<Point>();
+        assertNull(ray.findClosestPoint(points2), "Found point is not as the expected one");
+
+        //TC02: Point in the beginning of list
+        List<Point> points3 = new ArrayList<Point>();
+        points3.add(p1);
+        points3.add(p2);
+        points3.add(p3);
+        assertEquals(p1, ray.findClosestPoint(points3), "Found point is not as the expected one");
+
+        //TC03: Point in the end of list
+        List<Point> points4 = new ArrayList<Point>();
+        points4.add(p2);
+        points4.add(p3);
+        points4.add(p1);
+        assertEquals(p1, ray.findClosestPoint(points4), "Found point is not as the expected one");
     }
 }
