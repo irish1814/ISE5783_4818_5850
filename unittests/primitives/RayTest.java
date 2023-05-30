1f3266d03c2b03cc7e7cb9412f8899d71429fe94
irish1814 <101556047+irish1814@users.noreply.github.com>
2023-05-30 16:22:36 +0300

    continue stage 5 added all tests to findClosestPoint in RayTest class

--- unittests/primitives/RayTest.java
+++ unittests/primitives/RayTest.java
@@ -28,30 +28,30 @@ class RayTest {
         // ============ Equivalence Partitions Tests ==============
 
         //TC01: Point in the middle of list
-        List<Point> points1 = new LinkedList<>();
-        points1.add(p2);
-        points1.add(p1);
-        points1.add(p3);
-        assertEquals(p1, ray.findClosestPoint(points1), "Found point is not as the expected one");
+        List<Point> middleOfList = new LinkedList<>();
+        middleOfList.add(p2);
+        middleOfList.add(p1);
+        middleOfList.add(p3);
+        assertEquals(p1, ray.findClosestPoint(middleOfList), "Found point is not as the expected one");
 
         // =============== Boundary Values Tests ==================
 
         //TC01: List is empty
-        List<Point> points2 = new LinkedList<>();
-        assertNull(ray.findClosestPoint(points2), "Found point is not as the expected one");
+        List<Point> emptyList = new LinkedList<>();
+        assertNull(ray.findClosestPoint(emptyList), "Found point is not as the expected one");
 
         //TC02: Point in the beginning of list
-        List<Point> points3 = new LinkedList<>();
-        points3.add(p1);
-        points3.add(p2);
-        points3.add(p3);
-        assertEquals(p1, ray.findClosestPoint(points3), "Found point is not as the expected one");
+        List<Point> startOfList = new LinkedList<>();
+        startOfList.add(p1);
+        startOfList.add(p2);
+        startOfList.add(p3);
+        assertEquals(p1, ray.findClosestPoint(startOfList), "Found point is not as the expected one");
 
         //TC03: Point in the end of list
-        List<Point> points4 = new LinkedList<>();
-        points4.add(p2);
-        points4.add(p3);
-        points4.add(p1);
-        assertEquals(p1, ray.findClosestPoint(points4), "Found point is not as the expected one");
+        List<Point> endOfList = new LinkedList<>();
+        endOfList.add(p2);
+        endOfList.add(p3);
+        endOfList.add(p1);
+        assertEquals(p1, ray.findClosestPoint(endOfList), "Found point is not as the expected one");
     }
 }
