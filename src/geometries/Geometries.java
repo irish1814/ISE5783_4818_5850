b7b6d47cf29f8555965d7588ab6d7e1cf8875bf4
elad3434 <elad3434@gmail.com>
2023-06-19 19:26:50 +0300

    finish stage 6

--- src/geometries/Geometries.java
+++ src/geometries/Geometries.java
@@ -6,16 +6,32 @@ import primitives.Ray;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * A class represents group of geometry's shapes
+ *
+ * @author Ishay Houri & Elad Radomski
+ */
 public class Geometries extends Intersectable {
     private final List<Intersectable> geometries = new LinkedList<>();
 
-    public Geometries() {
-    }
+    /**
+     * default constructor to build the list with
+     * initialized values
+     */
+    public Geometries() {}
 
+    /**
+     * constructor that get geometries and add them to the list
+     * @param geometries to build the list
+     */
     public Geometries(Intersectable... geometries) {
         add(geometries);
     }
 
+    /**
+     * add geometries to the list
+     * @param geometries the shapes to add to the list
+     */
     public void add(Intersectable... geometries) {
         if (geometries != null) {
             this.geometries.addAll(List.of(geometries));
--- src/geometries/Intersectable.java
+++ src/geometries/Intersectable.java
@@ -9,7 +9,7 @@ import java.util.List;
 import static java.util.stream.Collectors.toList;
 
 /**
- * abstract class represents all intersection points of a geometric shape
+ * abstract classm represents all intersection points of a geometric shape
  *
  * @author Ishay Houri & Elad Radomski
  */
